b416cefa876a53e497642b002885ecc36dc7ac67
Florian Kuepper <florian.kuepper@googlemail.com>
2012-02-24 16:40:52 +0100

    Added empty credits-screen.

---
+++ Vocaluxe/Screens/CScreenCredits.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+using Vocaluxe.Base;
+using Vocaluxe.Lib.Draw;
+using Vocaluxe.Menu;
+
+namespace Vocaluxe.Screens
+{
+    class CScreenCredits :CMenu
+    {
+        // Version number for theme files. Increment it, if you've changed something on the theme files!
+        const int ScreenVersion = 1;
+
+        public CScreenCredits()
+        {
+            Init();
+        }
+
+        protected override void Init()
+        {
+            base.Init();
+
+            _ThemeName = "ScreenCredits";
+            _ScreenVersion = ScreenVersion;
+        }
+
+        public override bool HandleInput(KeyEvent KeyEvent)
+        {
+            if (KeyEvent.KeyPressed && !Char.IsControl(KeyEvent.Unicode))
+            {
+
+            }
+            else
+            {
+                switch (KeyEvent.Key)
+                {
+                    case Keys.Escape:
+                    case Keys.Back:
+                        CGraphics.FadeTo(EScreens.ScreenMain);
+                        break;
+
+                    case Keys.Enter:
+                        CGraphics.FadeTo(EScreens.ScreenMain);
+                        break;
+                }
+            }
+
+            return true;
+        }
+
+        public override bool HandleMouse(MouseEvent MouseEvent)
+        {
+            if (MouseEvent.LB && IsMouseOver(MouseEvent))
+            {
+
+            }
+
+            if (MouseEvent.LB)
+            {
+                CGraphics.FadeTo(EScreens.ScreenMain);
+            }
+
+            if (MouseEvent.RB)
+            {
+                CGraphics.FadeTo(EScreens.ScreenMain);
+            }
+            return true;
+        }
+
+        public override bool UpdateGame()
+        {
+
+            return true;
+        }
+
+        public override void OnShow()
+        {
+            base.OnShow();
+
+        }
+
+        public override bool Draw()
+        {
+            return base.Draw();
+        }
+    }
+}
