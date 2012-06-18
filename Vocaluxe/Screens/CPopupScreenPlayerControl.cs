9b50c7e5633f0d7b8df38df7404207355722aad2
Alexander Eckart <brunzel@gmx.li>
2012-06-18 18:18:02 +0200

    fixed small bug

--- Vocaluxe/Screens/CPopupScreenPlayerControl.cs
+++ Vocaluxe/Screens/CPopupScreenPlayerControl.cs
@@ -218,7 +218,7 @@ namespace Vocaluxe.Screens
                 CGame.ClearSongs();
 
                 EGameMode gm = EGameMode.Normal;
-                if (CSongs.VisibleSongs[SongNr].IsDuet)
+                if (CSongs.AllSongs[SongNr].IsDuet)
                     gm = EGameMode.Duet;
 
                 CGame.AddSong(SongNr, gm);
