91dd3a550c4f465eb93218b551a9c8bd8e8ff3cb
Stephan Sundermann <stephansundermann@gmail.com>
2012-09-19 20:41:17 +0200

    Fixed detection

--- Vocaluxe/Lib/Draw/CDirect3D.cs
+++ Vocaluxe/Lib/Draw/CDirect3D.cs
@@ -187,9 +187,9 @@ namespace Vocaluxe.Lib.Draw
 
             //Check if Pow2 textures are needed
             _NonPowerOf2TextureSupported = true;
-            _NonPowerOf2TextureSupported &= (caps.TextureCaps & TextureCaps.Pow2) != 0;
-            _NonPowerOf2TextureSupported &= (caps.TextureCaps & TextureCaps.NonPow2Conditional) != 0;
-            _NonPowerOf2TextureSupported &= (caps.TextureCaps & TextureCaps.SquareOnly) != 0;
+            _NonPowerOf2TextureSupported &= (caps.TextureCaps & TextureCaps.Pow2) == 0;
+            _NonPowerOf2TextureSupported &= (caps.TextureCaps & TextureCaps.NonPow2Conditional) == 0;
+            _NonPowerOf2TextureSupported &= (caps.TextureCaps & TextureCaps.SquareOnly) == 0;
 
             try
             {
