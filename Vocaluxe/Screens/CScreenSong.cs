9d5fdb67de83373829dbd8ca85087f2a0f6ff49d
Alexander Eckart <brunzel@gmx.li>
2012-12-02 15:02:02 +0100

    fixed ButtonOpenOptions handling in song screen

--- Vocaluxe/Screens/CScreenSong.cs
+++ Vocaluxe/Screens/CScreenSong.cs
@@ -655,6 +655,7 @@ namespace Vocaluxe.Screens
                 Buttons[htButtons(ButtonOptionsClose)].Visible = true;
                 Texts[htTexts(TextOptionsTitle)].Visible = true;
                 Statics[htStatics(StaticOptionsBG)].Visible = true;
+                Buttons[htButtons(ButtonOpenOptions)].Visible = false;
             }
             else 
             {
@@ -668,6 +669,7 @@ namespace Vocaluxe.Screens
                 Buttons[htButtons(ButtonOptionsSingAll)].Visible = false;
                 Buttons[htButtons(ButtonOptionsSingAllVisible)].Visible = false;
                 Buttons[htButtons(ButtonOptionsOpenPlaylist)].Visible = false;
+                Buttons[htButtons(ButtonOpenOptions)].Visible = true;
                 Texts[htTexts(TextOptionsTitle)].Visible = false;
                 Statics[htStatics(StaticOptionsBG)].Visible = false;
             }
