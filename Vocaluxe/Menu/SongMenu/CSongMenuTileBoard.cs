99ce2ca60fa7224d5f700255819eebed9551d597
Florian Kuepper <florian.kueper@googlemail.com>
2012-12-09 10:24:50 +0100

    Fix drawing of vid-texture with reflection in song-menu.

--- Vocaluxe/Menu/SongMenu/CSongMenuTileBoard.cs
+++ Vocaluxe/Menu/SongMenu/CSongMenuTileBoard.cs
@@ -465,10 +465,11 @@ namespace Vocaluxe.Menu.SongMenu
                 }
             }
 
+            _TextBG.Draw();
+
             _CoverBig.Draw(1f, true);
             if (_vidtex.color.A < 1)
                 _CoverBig.Draw(1f, true);
-            _TextBG.Draw();
 
             if (_vidtex.index != -1 && _Video != -1)
             {
