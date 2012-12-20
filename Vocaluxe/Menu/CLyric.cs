327087bfe63329af2a177fcdde0323f3618b6460
Florian Kuepper <florian.kueper@googlemail.com>
2012-12-20 17:00:23 +0100

    Fix jump-style of lyrics.

--- Vocaluxe/Menu/CLyric.cs
+++ Vocaluxe/Menu/CLyric.cs
@@ -506,7 +506,7 @@ namespace Vocaluxe.Menu
                 x += rect.Width;
             }
 
-            if (jump_note > 0)
+            if (jump_note > -1)
             {
                 if (_Notes[jump_note].Duration == 0)
                     return;
