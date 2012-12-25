2cb43ae7536ccf3da28951da2a8e616fbb381c62
Florian Küpper <florian.kuepper@googlemail.com>
2012-12-25 22:34:55 +0100

    Fix search.

--- Vocaluxe/Screens/CScreenSong.cs
+++ Vocaluxe/Screens/CScreenSong.cs
@@ -789,13 +789,13 @@ namespace Vocaluxe.Screens
             }
 
             _sso = CParty.GetSongSelectionOptions();
-            CSongs.Sort(_sso.Sorting.SongSorting, _sso.Sorting.Tabs, _sso.Sorting.IgnoreArticles, _sso.Sorting.SearchString, _sso.Sorting.ShowDuetSongs);
-            _SearchActive = _sso.Sorting.SearchStringVisible;
-            _SearchText = _sso.Sorting.SearchString;
  
 
             if (_sso.Selection.PartyMode)
             {
+                CSongs.Sort(_sso.Sorting.SongSorting, _sso.Sorting.Tabs, _sso.Sorting.IgnoreArticles, _sso.Sorting.SearchString, _sso.Sorting.ShowDuetSongs);
+                _SearchActive = _sso.Sorting.SearchStringVisible;
+                _SearchText = _sso.Sorting.SearchString;
                 ClosePlaylist();
                 Playlists[htPlaylists(Playlist)].ClosePlaylist();
                 ToggleSongOptions(ESongOptionsView.None);
