f0f751dcad89d850e71c0cc5e5386b27785841d6
Florian Küpper <florian.kuepper@gmail.com>
2013-08-08 10:48:20 +0200

    Added converter for language-files and *.xml files for easier translation with Transifex.

---
+++ Installer/nshToXmlConverter/nshToXmlConverter/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nshToXmlConverter
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OnDragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            List<string> filesList = new List<string>();
+            filesList.AddRange(files);
+            foreach (string file in filesList)
+            {
+                string extension = Path.GetExtension(file).ToLower();
+
+                switch (extension)
+                {
+                    case ".xml":
+                        if (!CConverter.ConvertXmlToNsh(file))
+                            MessageBox.Show("Can't convert " + Path.GetFileName(file) + " to nsh.");
+                        break;
+
+                    case ".nsh":
+                        if(!CConverter.ConvertNshToXml(file))
+                            MessageBox.Show("Can't convert " + Path.GetFileName(file) + " to xml.");
+                        break;
+                }
+            }
+        }
+
+        private void OnDragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false) == true)
+            {
+                e.Effect = DragDropEffects.All;
+            }
+        }
+    }
+}
