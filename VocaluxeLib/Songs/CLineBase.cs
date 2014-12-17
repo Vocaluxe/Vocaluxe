10b666a1bf22c5606594fba26d7dd98469e7e65e
Flamefire <git@grundis.de>
2014-12-17 21:50:28 +0100

    Fix jumping note drawing

--- VocaluxeLib/Songs/CLineBase.cs
+++ VocaluxeLib/Songs/CLineBase.cs
@@ -75,10 +75,10 @@ namespace VocaluxeLib.Songs
 
         #region Methods
         /// <summary>
-        ///     Find last note with StartBeat
+        ///     Find last note with StartBeat &lt;= beat
         /// </summary>
         /// <param name="beat"></param>
-        /// <returns>Note index of last note with StartBeat or -1 if all notes start after beat</returns>
+        /// <returns>Note index of last note with StartBeat &lt;= beat or -1 if all notes start after beat</returns>
         public int FindPreviousNote(int beat)
         {
             //If no notes -> No previous note
