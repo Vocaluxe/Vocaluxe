ced828c4016bcb002012b81304ba83e17d1886c5
lukeIam <mail2skywalker@gmail.com>
2015-10-05 22:13:34 +0200

    Appveyor: add prepareDeploy.ps1 and gitDescribe.ps1 which is needed to use GitHub API to correctly tag automatic builds, because git-describe doesn't work on shallow clones

---
+++ .appveyor/gitDescribe.ps1
@@ -0,0 +1,54 @@
+$currentCommitSha = $Env:CurrentCommitSha
+$githubRepoApiUri = $Env:githubRepoApiUri #"https://api.github.com/repos/:user/:repo/"
+$Env:VersionTag = ""
+
+# Inline-if helper function
+Function IIf($If, $Right, $Wrong) {If ($If) {$Right} Else {$Wrong}}
+
+try{
+	$result = Invoke-RestMethod -Uri "$($githubRepoApiUri)git/refs/tags" -Method 'GET'
+}
+catch [System.Net.WebException] 
+{	
+	# Error getting tags
+    $statusCode = [int]$_.Exception.Response.StatusCode
+    $html = $_.Exception.Response.StatusDescription
+	Write-Host "Error getting tags ($html)"
+	Return ;
+}
+
+# Get tag info
+try{
+	$tagsInfo = ($result | % { "$($githubRepoApiUri)git/tags/$($_.object.sha)" ;} | % { Invoke-RestMethod -Method 'GET' -Uri $($_)})
+}
+catch [System.Net.WebException] 
+{	
+	# Error getting tags
+    $statusCode = [int]$_.Exception.Response.StatusCode
+    $html = $_.Exception.Response.StatusDescription
+	Write-Host "Error getting tags ($html)"
+	Return ;
+}
+
+# Compare taged commits with current commit
+try{
+	$commitTagComp = $tagsInfo | select sha, tag, tagger, @{n='dist';e={[int](Invoke-RestMethod -Method 'GET' -Uri "$($githubRepoApiUri)compare/$($_.object.sha)...$currentCommitSha" | % { (IIf ($_.status -ne "diverged") ($_.ahead_by) (-1))})}}
+}
+catch [System.Net.WebException] 
+{	
+	# Error comparing tags
+    $statusCode = [int]$_.Exception.Response.StatusCode
+    $html = $_.Exception.Response.StatusDescription
+	Write-Host "Error comparing tags ($html)"
+	Return ;
+}
+
+$latestTag = $commitTagComp | where {$_.dist -ge 0} | sort-object -property @{Expression="dist";Descending=$false}, @{Expression="tag";Descending=$false} | Select-Object -first 1
+
+if(!$latestTag){
+	Write-Host "Commit not describable"	
+	Return ;
+}
+
+$Env:VersionTag = "$($latestTag.tag)-$($latestTag.dist)-g$($currentCommitSha.Substring(0,7))"
+Return $Env:VersionTag
---
+++ .appveyor/prepareDeploy.ps1
@@ -0,0 +1,65 @@
+if($Env:APPVEYOR_REPO_TAG -eq "true") {
+	$targetTag = $Env:APPVEYOR_REPO_TAG_NAME
+	Write-Host "Tag=$targetTag"
+}
+else{
+	if($Env:APPVEYOR_REPO_BRANCH -eq "appveyorTest"){		
+		$targetTag = "Nightly"		
+	}
+	else{
+		Write-Host "No tag or special branch found"
+		Return
+	}
+	git config --global user.email "build@vocaluxe.de";
+	git config --global user.name "Vocaluxe_Automatic_Build";
+	git tag "$targetTag" -f;
+	git push -q -f "https://$($Env:GitHubKey):x-oauth-basic@github.com/lukeIam/Vocaluxe.git" "$targetTag";
+	
+	Write-Host "Branch $Env:APPVEYOR_REPO_BRANCH -> Tag=$targetTag"
+}
+
+$getRelaseInfoParams = @{
+	Uri = "https://api.github.com/repos/lukeIam/Vocaluxe/releases/tags/$targetTag";
+	Method = 'GET';   
+}
+try{
+	$result = Invoke-RestMethod @getRelaseInfoParams 
+}
+catch [System.Net.WebException] 
+{
+	# No Release found for this tag
+    $statusCode = [int]$_.Exception.Response.StatusCode
+    $html = $_.Exception.Response.StatusDescription
+	Write-Host "No Release found for this tag ($html)"
+	Return
+}
+
+$assetId = ($result.assets | where {$_.name -EQ "Vocaluxe_$Env:APPVEYOR_REPO_BRANCH_Windows_$Env:PLATFORM.zip" }  | Select-Object -first 1 ).id
+
+if(!$assetId){
+	# No matching asset found in this release
+	Write-Host "No matching asset found in this release"
+	Return
+}
+
+$deleteAssetParams = @{
+	Uri = "https://api.github.com/repos/lukeIam/Vocaluxe/releases/assets/$assetId";
+	Method = 'DELETE';
+	Headers = @{
+		Authorization = 'Basic ' + [Convert]::ToBase64String(
+		[Text.Encoding]::ASCII.GetBytes($Env:GitHubKey + ":x-oauth-basic"));
+	}
+}
+try{
+	$result = Invoke-RestMethod @deleteAssetParams 
+	Write-Host "Successfully deleted asset"
+}
+catch [System.Net.WebException] 
+{
+	# Could not delete asset
+    $statusCode = [int]$_.Exception.Response.StatusCode
+    $html = $_.Exception.Response.StatusDescription
+	Write-Host "Could not delete asset ($html)"
+	Return
+}
+ 
