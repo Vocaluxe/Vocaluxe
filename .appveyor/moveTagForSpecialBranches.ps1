80516570a6d318d10c057b9bddcfa5608ea7ecfe
lukeIam <2lukeiam@gmail.com>
2015-11-27 21:26:38 +0100

    Add more logging to moveTagForSpecialBranches.ps1 for debugging

--- .appveyor/moveTagForSpecialBranches.ps1
+++ .appveyor/moveTagForSpecialBranches.ps1
@@ -1,5 +1,8 @@
+
 if($Env:APPVEYOR_REPO_TAG -ne "true") {
+    Write-Host "Pushed tag detected"
 	if($Env:APPVEYOR_REPO_BRANCH -eq "appveyorTest"){
+        Write-Host "Nightly build tag was detected"
 		$targetTag = "Nightly"	
 		$Env:NIGHTLY_BUILD = true		
 	}
@@ -7,10 +10,15 @@ if($Env:APPVEYOR_REPO_TAG -ne "true") {
 		Write-Host "No special branch found"
 		Return
 	}
+    Write-Host "Moving tag $targetTag to $Env:APPVEYOR_REPO_BRANCH"
 	git config --global user.email "build@vocaluxe.de";
 	git config --global user.name "Vocaluxe_Automatic_Build";
 	git tag "$targetTag" -f;
 	git push -q -f "https://$($Env:GitHubKey):x-oauth-basic@github.com/lukeIam/Vocaluxe.git" "$targetTag";
 	
 	Write-Host "Tag $targetTag points now to the head of branch $Env:APPVEYOR_REPO_BRANCH"
+}
+else
+{
+    Write-Host "No tag for this detected"
 }
