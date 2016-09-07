29352278fa70113d6fe5b9960e13dbe1ba89a80d
Jiiniasu <jiiniasu@gief.info>
2016-09-07 18:41:44 +0200

    Fix Typo in Query
    BIGING -> BIGINT

--- Vocaluxe/Lib/Database/CHighscoreDB.cs
+++ Vocaluxe/Lib/Database/CHighscoreDB.cs
@@ -503,7 +503,7 @@ namespace Vocaluxe.Lib.Database
                     command.ExecuteNonQuery();
 
                     command.CommandText = "CREATE TABLE IF NOT EXISTS Songs ( id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
-                                          "Artist TEXT NOT NULL, Title TEXT NOT NULL, NumPlayed INTEGER, DateAdded BIGING);";
+                                          "Artist TEXT NOT NULL, Title TEXT NOT NULL, NumPlayed INTEGER, DateAdded BIGINT);";
                     command.ExecuteNonQuery();
 
                     command.CommandText = "CREATE TABLE IF NOT EXISTS Scores ( id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
