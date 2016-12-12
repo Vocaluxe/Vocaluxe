9d4a420ea2987370032f8cda0d4e3de7b422c000
lukeIam <2lukeiam@gmail.com>
2016-12-12 21:01:41 +0100

    -Mod: New versioning scheme including type if type!=release -Mod: Show the same version tag everywhere -Mod: Automatically determinate release type depending on the new version-tag -Mod: Automatically determinate CodeName -Add: Code-name for the 0.4. releases

--- Vocaluxe/Linux/Build/PostBuildEvent.sh
+++ Vocaluxe/Linux/Build/PostBuildEvent.sh
@@ -2,4 +2,6 @@
 PROJECT=$1
 cd $PROJECT
 sed -i -r -e 's/AssemblyInformationalVersion\(".*?"\)/AssemblyInformationalVersion("GITVERSION")/' $PROJECT/Properties/AssemblyInfo.cs
+sed -i -r -e 's/AssemblyVersion\(".*?"\)/AssemblyVersion("SHORTVERSION")/' $PROJECT/Properties/AssemblyInfo.cs
+sed -i -r -e 's/AssemblyFileVersion\(".*?"\)/AssemblyFileVersion("SHORTVERSION")/' $PROJECT/Properties/AssemblyInfo.cs
 sed -i -e 's/var matchingServerVersion = "";/var matchingServerVersion = "'$(git describe --long)'";/' $PROJECT/../Output/Website/js/main.js
