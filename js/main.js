4c47d86fdc866b59566c6b0100bb575b43a10dad
Florian Küpper <f.kuepper@uni-muenster.de>
2016-12-18 19:48:13 +0100

    Some first work on structure

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//Contact Form
+
+$('#submit').click(function(){
+
+$.post("assets/php/send.php", $(".contact-form").serialize(),  function(response) {   
+ $('#success').html(response);
+});
+return false;
+
+});
