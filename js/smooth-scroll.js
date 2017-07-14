d7517b170a5d4f2f316db963b876221323d687bf
Hendrik Blöß <hendrik.bloess@googlemail.com>
2017-07-14 17:37:56 +0200

    Added gallery section
    - added gallery section
    - removed hero_area_small.html

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
+  $('a[href*=#]:not([href=#]):not(.control-button)').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
