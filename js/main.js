74f31e7b8b53d1230e9cd8b730d6890b8fe8ef4d
Hendrik Blöß <hendrik.bloess@googlemail.com>
2017-08-09 13:34:42 +0200

    review changes
    - removed unused fonts (arch, glyphicon)
    - made news image clickable
    - fixed gallery image-size for for IE11
    - removed unused js function

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-//Contact Form
-$('#submit').click(function() {
-
-  $.post("assets/php/send.php", $(".contact-form").serialize(), function(response) {
-    $('#success').html(response);
-  });
-  return false;
-
-});
-
 //Swipebox
 //Use .swipebox class
 (function($) {
