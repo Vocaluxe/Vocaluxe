03268ecab420f41f0a844478d8e5da5e0eec0d63
lukeIam <2lukeiam@gmail.com>
2017-08-14 19:34:18 +0200

    Add more info about the triggering commit to the "dummy" gh-pages commit message.

--- .appveyor/afterDeploy.ps1
+++ .appveyor/afterDeploy.ps1
@@ -3,6 +3,6 @@ git config --global user.email "noreply@vocaluxe.org"
 git config --global user.name "VocaluxeBuildBot"
 git checkout "gh-pages"
 git reset --hard HEAD
-git commit --allow-empty -m "Trigger page update after a new publish"
+git commit --allow-empty -m "Trigger page update after a new publish (triggert by $Env:APPVEYOR_REPO_COMMIT_MESSAGE ($Env:APPVEYOR_REPO_COMMIT))"
 git push -q "https://$($Env:GitHubKey):x-oauth-basic@github.com/Vocaluxe/Vocaluxe.git" "gh-pages"
 git checkout -
