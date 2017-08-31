3bb8fa90bc0294339dec9af27d246076e2891ae5
Hendrik Blöß <hendrik.bloess@googlemail.com>
2017-08-31 19:21:00 +0200

    Download Section Prototype
    - very early testing of new download cards

--- js/main.js
+++ js/main.js
@@ -2,6 +2,34 @@
 //Use .swipebox class
 (function($) {
 
-  $('.swipebox').swipebox();
+  $(".swipebox").swipebox();
 
 })(jQuery);
+
+//Download Button
+
+function showCard(card) {
+  card.addClass("animated flipInY").removeClass("flipOutY hidden");
+}
+
+function hideCard(card) {
+  card.addClass("flipOutY").removeClass("flipInY");
+}
+
+// Nightlies
+$(".dark-bg .btnDownload").click(function() {
+  showCard($(".dark-bg .card-overlay"));
+});
+
+$(".dark-bg .card-overlay .close-icon").click(function () {
+  hideCard($(".dark-bg .card-overlay"));
+});
+
+// Pre Release
+$(".red-bg .btnDownload").click(function() {
+  showCard($(".red-bg .card-overlay"));
+});
+
+$(".red-bg .card-overlay .close-icon").click(function () {
+  hideCard($(".red-bg .card-overlay"));
+});
