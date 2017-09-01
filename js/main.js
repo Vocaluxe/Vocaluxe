f40e3ed4ea304328e55588abf9a60f746e44cd56
Hendrik <hendrik.bloess@googlemail.com>
2017-09-01 20:12:58 +0200

    added installer download option
    - new download card
    - added buttons for each release file
    - improved general responsiveness

--- js/main.js
+++ js/main.js
@@ -2,6 +2,43 @@
 //Use .swipebox class
 (function($) {
 
-  $('.swipebox').swipebox();
+  $(".swipebox").swipebox();
 
 })(jQuery);
+
+//Download Cards
+
+function showCard(card) {
+  card.addClass("animated flipInY").removeClass("flipOutY hidden");
+}
+
+function hideCard(card) {
+  card.addClass("flipOutY").removeClass("flipInY");
+}
+
+// Nightlies
+$(".dark-bg .btnDownload").click(function() {
+  showCard($(".dark-bg .card-overlay"));
+});
+
+$(".dark-bg .card-overlay .close-icon").click(function () {
+  hideCard($(".dark-bg .card-overlay"));
+});
+
+// Pre Release
+$(".red-bg .btnDownload").click(function() {
+  showCard($(".red-bg .card-overlay"));
+});
+
+$(".red-bg .card-overlay .close-icon").click(function () {
+  hideCard($(".red-bg .card-overlay"));
+});
+
+// Stable
+$(".blue-bg .btnDownload").click(function() {
+  showCard($(".blue-bg .card-overlay"));
+});
+
+$(".blue-bg .card-overlay .close-icon").click(function () {
+  hideCard($(".blue-bg .card-overlay"));
+});
