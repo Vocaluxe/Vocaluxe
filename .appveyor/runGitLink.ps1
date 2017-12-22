5578082a6d70615f2a2bf2f2e1cf2d887cef380e
lukeIam <2lukeiam@gmail.com>
2017-12-22 14:25:46 +0100

    - Less output while installing gitlink - Try to fix runGitLink.ps1 errors

--- .appveyor/runGitLink.ps1
+++ .appveyor/runGitLink.ps1
@@ -1,10 +1,10 @@
-choco install gitlink --version 3.2.0-unstable0014 --pre
+choco install gitlink --version 3.2.0-unstable0014 --pre --no-progress --limit-output
 
 Get-ChildItem -Recurse "*.pdb" | %{
     If ($_.Fullname -contains 'PitchTracker.pdb'){
-        gitlink -u "https://github.com/$($Env:APPVEYOR_REPO_NAME)" -a --baseDir "$Env:APPVEYOR_BUILD_FOLDER" --commit "$Env:APPVEYOR_REPO_COMMIT" $_.Fullname
+        & echo -u "`"https://github.com/$($Env:APPVEYOR_REPO_NAME)`"" -a --baseDir "`"$Env:APPVEYOR_BUILD_FOLDER`"" --commit "`"$Env:APPVEYOR_REPO_COMMIT`"" "`"$($_.Fullname)`""
     }
     else{
-        gitlink -u "https://github.com/$($Env:APPVEYOR_REPO_NAME)" --baseDir "$Env:APPVEYOR_BUILD_FOLDER" --commit "$Env:APPVEYOR_REPO_COMMIT" $_.Fullname
+        & echo -u "`"https://github.com/$($Env:APPVEYOR_REPO_NAME)`"" --baseDir "`"$Env:APPVEYOR_BUILD_FOLDER`"" --commit "`"$Env:APPVEYOR_REPO_COMMIT`"" "`"$($_.Fullname)`""
     }    
 }
