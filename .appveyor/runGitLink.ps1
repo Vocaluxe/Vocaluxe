2a62e69a741b73fb0898b87fa908175b9cacc416
lukeIam <2lukeiam@gmail.com>
2017-12-22 14:29:29 +0100

    Removed debug code from runGitLink.ps1

--- .appveyor/runGitLink.ps1
+++ .appveyor/runGitLink.ps1
@@ -2,9 +2,9 @@ choco install gitlink --version 3.2.0-unstable0014 --pre --no-progress --limit-o
 
 Get-ChildItem -Recurse "*.pdb" | %{
     If ($_.Fullname -contains 'PitchTracker.pdb'){
-        & echo -u "`"https://github.com/$($Env:APPVEYOR_REPO_NAME)`"" -a --baseDir "`"$Env:APPVEYOR_BUILD_FOLDER`"" --commit "`"$Env:APPVEYOR_REPO_COMMIT`"" "`"$($_.Fullname)`""
+        & gitlink -u "`"https://github.com/$($Env:APPVEYOR_REPO_NAME)`"" -a --baseDir "`"$Env:APPVEYOR_BUILD_FOLDER`"" --commit "`"$Env:APPVEYOR_REPO_COMMIT`"" "`"$($_.Fullname)`""
     }
     else{
-        & echo -u "`"https://github.com/$($Env:APPVEYOR_REPO_NAME)`"" --baseDir "`"$Env:APPVEYOR_BUILD_FOLDER`"" --commit "`"$Env:APPVEYOR_REPO_COMMIT`"" "`"$($_.Fullname)`""
+        & gitlink -u "`"https://github.com/$($Env:APPVEYOR_REPO_NAME)`"" --baseDir "`"$Env:APPVEYOR_BUILD_FOLDER`"" --commit "`"$Env:APPVEYOR_REPO_COMMIT`"" "`"$($_.Fullname)`""
     }    
 }
