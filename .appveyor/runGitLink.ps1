20f1f98e22093fa272a9823693bfd8600665ce44
lukeIam <2lukeiam@gmail.com>
2017-12-22 17:03:18 +0100

    Fix: runGitLink.ps1 not found

--- .appveyor/runGitLink.ps1
+++ .appveyor/runGitLink.ps1
@@ -1,6 +1,6 @@
 choco install gitlink --version 3.1.0-unstable0017 --pre --no-progress --limit-output
 
-Get-ChildItem -Recurse "*.pdb" | %{
+Get-ChildItem -Path ".\Output" -Recurse "*.pdb" | %{
     If ($_.Fullname -contains 'PitchTracker.pdb'){
         & gitlink -u "`"https://github.com/$($Env:APPVEYOR_REPO_NAME)`"" -a --baseDir "`"$Env:APPVEYOR_BUILD_FOLDER`"" --commit "`"$Env:APPVEYOR_REPO_COMMIT`"" "`"$($_.Fullname)`""
     }
