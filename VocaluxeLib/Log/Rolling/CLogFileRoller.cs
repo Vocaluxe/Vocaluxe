e2397e743627fcc848bf6e12ea9cafd0269cee86
lukeIam <2lukeiam@gmail.com>
2018-03-24 11:37:54 +0100

    Fix: CLogFileRoller fails if a regex special char (like '.') is part of the directory name

--- VocaluxeLib/Log/Rolling/CLogFileRoller.cs
+++ VocaluxeLib/Log/Rolling/CLogFileRoller.cs
@@ -73,7 +73,7 @@ namespace VocaluxeLib.Log.Rolling
 
         private static void _DeleteOldLogs(string mainLogFile, int numLogsToKeep)
         {
-            Regex r = new Regex($"{Path.GetDirectoryName(mainLogFile)}\\{Path.DirectorySeparatorChar}{Path.GetFileNameWithoutExtension(mainLogFile)}_([0-9]+){Path.GetExtension(mainLogFile)}", RegexOptions.IgnoreCase);
+            Regex r = new Regex($"{Regex.Escape(Path.GetDirectoryName(mainLogFile)??"")}\\{Path.DirectorySeparatorChar}{Regex.Escape(Path.GetFileNameWithoutExtension(mainLogFile)??"")}_([0-9]+){Path.GetExtension(mainLogFile)}", RegexOptions.IgnoreCase);
             IEnumerable<string> logFiles = Directory.EnumerateFiles($"{Path.GetDirectoryName(mainLogFile)}", $"{Path.GetFileNameWithoutExtension(mainLogFile)}*{Path.GetExtension(mainLogFile)}");
             foreach (string file in logFiles)
             {
