77fa981751ad1b502f58a7a221ce99a7cea37107
lukeIam <2lukeiam@gmail.com>
2018-04-30 16:30:41 +0200

    Allow to skip the post build script

--- .build/winPostBuild.ps1
+++ .build/winPostBuild.ps1
@@ -3,6 +3,12 @@ param(
     [String]$ProjectDir
 )
 
+if($Env:VOCALUXE_SKIP_POSTBUILD)
+{
+    Write-Host "Skipping post build script (as requested)"
+    exit
+}
+
 (Get-Content -Encoding UTF8 "$($ProjectDir)Properties\AssemblyInfo.cs") `
     | Foreach-Object {$_ -replace '(?<=AssemblyInformationalVersion\(\").*(?=\")', 'GITVERSION'} `
     | Foreach-Object {$_ -replace '(?<=(AssemblyVersion|AssemblyFileVersion)\(\").*(?=\")', 'SHORTVERSION'} `
