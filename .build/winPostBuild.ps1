8e3b68b2b71e3f91ba1166f7314b0a75066c8650
lukeIam <2lukeiam@gmail.com>
2018-05-04 14:29:16 +0200

    Fix wrong single quote escaping in winPostBuild.ps1

--- .build/winPostBuild.ps1
+++ .build/winPostBuild.ps1
@@ -12,5 +12,5 @@ if($Env:VOCALUXE_SKIP_POSTBUILD)
 (Get-Content -Encoding UTF8 "$($ProjectDir)Properties\AssemblyInfo.cs") `
     | Foreach-Object {$_ -replace '(?<=AssemblyInformationalVersion\(\").*(?=\")', '0.0.0-na-notversioned'} `
     | Foreach-Object {$_ -replace '(?<=(AssemblyVersion|AssemblyFileVersion)\(\").*(?=\")', '0.0.0'} `
-    | Foreach-Object {$_ -replace '(?<=AssemblyTitle\(\").*(?=\")', 'Vocaluxe `'Not Versioned`' 0.0.0 (NA) (0.0.0-na-notversioned)'} `
+    | Foreach-Object {$_ -replace '(?<=AssemblyTitle\(\").*(?=\")', "Vocaluxe 'Not Versioned' 0.0.0 (NA) (0.0.0-na-notversioned)"} `
     | Set-Content -Encoding UTF8 "$($ProjectDir)Properties\AssemblyInfo.cs"
