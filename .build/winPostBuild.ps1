1a1d1fd6edd98cf0afc3a0cafab946bb4d343b2c
lukeIam <2lukeiam@gmail.com>
2018-05-04 14:22:54 +0200

    Replace old version dummy entries with such that fulfill the constraints (they will still be overwritten in the pre-build event) + change the build scripts not not longer depend on the dummy strings

--- .build/winPostBuild.ps1
+++ .build/winPostBuild.ps1
@@ -17,7 +17,7 @@ else
     | Set-Content -Encoding UTF8 "$($ProjectDir)..\Output\Website\js\main.js"
 
 (Get-Content -Encoding UTF8 "$($ProjectDir)Properties\AssemblyInfo.cs") `
-    | Foreach-Object {$_ -replace '(?<=AssemblyInformationalVersion\(\").*(?=\")', 'GITVERSION'} `
-    | Foreach-Object {$_ -replace '(?<=(AssemblyVersion|AssemblyFileVersion)\(\").*(?=\")', 'SHORTVERSION'} `
-    | Foreach-Object {$_ -replace '(?<=AssemblyTitle\(\").*(?=\")', 'FULLVERSION'} `
+    | Foreach-Object {$_ -replace '(?<=AssemblyInformationalVersion\(\").*(?=\")', '0.0.0-na-notversioned'} `
+    | Foreach-Object {$_ -replace '(?<=(AssemblyVersion|AssemblyFileVersion)\(\").*(?=\")', '0.0.0'} `
+    | Foreach-Object {$_ -replace '(?<=AssemblyTitle\(\").*(?=\")', 'Vocaluxe `'Not Versioned`' 0.0.0 (NA) (0.0.0-na-notversioned)'} `
     | Set-Content -Encoding UTF8 "$($ProjectDir)Properties\AssemblyInfo.cs"
--- Vocaluxe/Properties/AssemblyInfo.cs
+++ Vocaluxe/Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die mit einer Assembly verknüpft sind.
 
-[assembly: AssemblyTitle("FULLVERSION")]
+[assembly: AssemblyTitle("Vocaluxe 'Not Versioned' 0.0.0 (NA) (0.0.0-na-notversioned)")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -31,6 +31,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0-na-notversioned")]
 
 // Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
 // für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
@@ -41,19 +45,6 @@ using System.Runtime.InteropServices;
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 
 [assembly: Guid("da631594-f891-4994-9869-c37e626e44f9")]
-
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-
-[assembly: AssemblyVersion("SHORTVERSION")]
-[assembly: AssemblyFileVersion("SHORTVERSION")]
-[assembly: AssemblyInformationalVersion("GITVERSION")]
-
 
 // Allow internal access from the Tests project
 [assembly:InternalsVisibleTo("Tests")]
