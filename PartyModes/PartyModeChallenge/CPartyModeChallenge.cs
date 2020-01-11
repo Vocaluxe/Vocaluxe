405e41da6749e691481ce165b8bc1b3775ee90a5
daggeg <daggeg@hotmail.com>
2020-01-11 12:49:38 +0100

    Update CPartyModeChallenge.cs

--- PartyModes/PartyModeChallenge/CPartyModeChallenge.cs
+++ PartyModes/PartyModeChallenge/CPartyModeChallenge.cs
@@ -428,7 +428,6 @@ namespace VocaluxeLib.PartyModes.Challenge
                     _ScreenSongOptions.Sorting.FilterPlaylistID = -1;
 
                     _ScreenSongOptions.Selection.SongIndex = -1;
-                    _ScreenSongOptions.Selection.CategoryIndex = -1;
                     _ScreenSongOptions.Selection.CategoryChangeAllowed = true;
                     break;
 
