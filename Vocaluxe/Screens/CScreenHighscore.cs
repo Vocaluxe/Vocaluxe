a29d88a7a2dbb7470d6c338a45b166a111f8269e
GRaff844 <ywbmhss185@endob.com>
2024-11-17 17:47:10 +0100

    Update CScreenHighscore.cs

--- Vocaluxe/Screens/CScreenHighscore.cs
+++ Vocaluxe/Screens/CScreenHighscore.cs
@@ -173,13 +173,6 @@ namespace Vocaluxe.Screens
             return true;
         }
 
-        private static int Clamp(int value, int min, int max)
-        {
-            if (value < min) return min;
-            if (value > max) return max;
-            return value;
-        }
-
         public override bool UpdateGame()
         {
             for (int p = 0; p < _NumEntrys; p++)
