39e7e96026a4c93183ef86de4aa529eecdb8f7bc
Edi316 <61267443+Edi316@users.noreply.github.com>
2025-04-21 00:45:36 +0200

    Add Elevating Sound

--- Vocaluxe/Screens/CScreenScore.cs
+++ Vocaluxe/Screens/CScreenScore.cs
@@ -270,8 +270,8 @@ namespace Vocaluxe.Screens
                 _ProgressBarSoundStream = -1;
             }
 
-            // Calculate duration: 10,000 points = 7 seconds
-            double duration = Math.Min(maxPoints / 10000.0, 1.0) * 7.0;
+            // Calculate duration: 10,000 points = 8.5 seconds
+            double duration = Math.Min(maxPoints / 10000.0, 1.0) * 8.5;
 
             // Play the sound
             _ProgressBarSoundStream = PlaySound(ESounds.ProgressBar, CConfig.GameMusicVolume);
