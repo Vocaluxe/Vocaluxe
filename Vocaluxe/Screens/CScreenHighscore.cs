87b95e1a53d317d697eb7174049f5a01e04b845f
Edi316 <61267443+Edi316@users.noreply.github.com>
2025-04-22 12:41:39 +0200

    Add SoundEffectVolume

--- Vocaluxe/Screens/CScreenHighscore.cs
+++ Vocaluxe/Screens/CScreenHighscore.cs
@@ -199,7 +199,7 @@ namespace Vocaluxe.Screens
 
                     if (_ParticleEffects[_ParticleEffectNew[p]].Visible && !_HasPlayedSound)
                     {
-                         _HighscoreStream = CScreenHighscore.PlaySound(ESounds.Highscore, CConfig.GameMusicVolume);
+                         _HighscoreStream = CScreenHighscore.PlaySound(ESounds.Highscore, CCConfig.SoundEffectVolume);
                          _HasPlayedSound = true;
                     }
                   }
