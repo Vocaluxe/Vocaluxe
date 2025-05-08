d7179b6ff1fcf00164ebcc95f3accd7f3f125cc3
Edi316 <61267443+Edi316@users.noreply.github.com>
2025-05-08 20:55:15 +0200

    Add AudioMode Vocals

--- Vocaluxe/Screens/CScreenSing.cs
+++ Vocaluxe/Screens/CScreenSing.cs
@@ -379,8 +379,10 @@ namespace Vocaluxe.Screens
 
                             float newTime = _CurrentTime + (keyEvent.Mod == EModifier.Shift ? 10f : 30f);
                             if (CSound.GetLength(_CurrentStream) < newTime)
+                            {
                                 newTime = CSound.GetLength(_CurrentStream) - 1f;
                                 CSound.SetPosition(_CurrentStream, newTime);
+                            }
                                 
                             if (CScreenSong.GetAudioMode() == EAudioMode.TR_AUDIOMODE_VOCALS)
                             {
