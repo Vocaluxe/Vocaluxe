f61e6627b68fdd7fff2fefd1f2ea83b0329a0044
Florian Schulze <flori.schulze@yahoo.de>
2025-05-26 22:35:29 +0200

    Update nightly versioning

--- .build/winPreBuild.ps1
+++ .build/winPreBuild.ps1
@@ -4,37 +4,19 @@ param(
     [Parameter(Mandatory=$true)]
     [String]$Arch
 )
+$commitHash = $Env:GITHUB_SHA;
 
-if($Env:GIT_VERSION_TAG)
-{
-    $version = $Env:GIT_VERSION_TAG;
-}
-else
-{
-    $version = git describe --long;
-}
+$shortVersion = "0.0.0"
+$unixTime = [DateTimeOffset]::Now.ToUnixTimeSeconds()
+$version = "$($shortVersion).$($unixTime)"
 
-$shortVersion = $version.Split('-')[0];
-
-[System.Reflection.Assembly]::LoadWithPartialName("System.Web.Extensions")
-$versionName = (New-Object System.Web.Script.Serialization.JavaScriptSerializer).Deserialize((Get-Content "$($ProjectDir)..\.build\versionNames.json"), `
-    [System.Collections.Hashtable])[([regex]"[0-9]+\.[0-9]+\.").Match($shortVersion).Value]
-
-$releaseType = switch -wildcard ($shortVersion.Split('.')[2]) 
-{ 
-    "alpha*" {"Alpha"} 
-    "beta*" {"Beta"} 
-    "rc*" {"RC"}         
-    default {"Release"}
-};
-
-$fullVersionName = "Vocaluxe $(if($versionName) {"'"+$versionName+"' "} else {''})$shortVersion ($Arch) $(if($releaseType -ne "Release") {'$releaseType '} else {''})($version)"
+$fullVersionName = "Vocaluxe $version ($Arch)"
 
 (Get-Content -Encoding UTF8 "$($ProjectDir)Properties\AssemblyInfo.cs") `
     | Foreach-Object {$_ `
         -replace '(?<=AssemblyInformationalVersion\(").*(?=")', $version `
         -replace '(?<=(AssemblyVersion|AssemblyFileVersion)\(").*(?=")', `
-            ($version.Split('-')[0].replace('alpha','0').replace('beta','1').replace('rc','2') -replace "[a-zA-Z]+","")`
+            ($version) `
         -replace '(?<=AssemblyTitle\(").*(?=")', $fullVersionName `
         -replace '(?<=AssemblyCopyright\(".*)[0-9]+(?=")', (Get-Date).Year} `
     | Set-Content -Encoding UTF8 "$($ProjectDir)Properties\AssemblyInfo.cs"
