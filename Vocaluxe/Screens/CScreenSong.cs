8cce3eb60b74596a09b99a9db4b0b606e3655371
Edi316 <61267443+Edi316@users.noreply.github.com>
2026-03-21 17:22:34 +0100

    Update SearchBar

--- Vocaluxe/Screens/CScreenSong.cs
+++ Vocaluxe/Screens/CScreenSong.cs
@@ -519,17 +519,17 @@ namespace Vocaluxe.Screens
 
 			if (mouseEvent.LB && Statics[StaticSearchBar].Visible && CHelper.IsInBounds(Statics[StaticSearchBar].Rect, mouseEvent))
 			{
-                if (SearchActive)
-                    {
-                       SearchActive = false;
-                       SearchText = String.Empty;
-                       ApplyNewSearchFilter(SearchText);
-                    }
-                else if (!Sso.Selection.PartyMode)
-                    {
-                       SearchActive = true;
-                    }
-                return true;
+			    if (SearchActive)
+			    {
+			        SearchActive = false;
+			        SearchText = String.Empty;
+			        ApplyNewSearchFilter(SearchText);
+			    }
+			    else if (!Sso.Selection.PartyMode)
+			    {
+			        SearchActive = true;
+			    }
+			    return true;
 			}
 
             if (_DragAndDropCover.Visible)
